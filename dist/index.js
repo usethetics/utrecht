4a62e66d10686811b200e150533933c54347f66f
Robbert <mail@robbertbroersma.nl>
2021-06-30 23:13:27 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@6741ddc4f4c70ee098caedef2304568e1ae402cd 🚀

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 30 Jun 2021 22:56:33 GMT
+ * Generated on Wed, 30 Jun 2021 23:12:33 GMT
  */
 
 export const utrechtBlockquoteMarginInlineStart = "1.6em";
