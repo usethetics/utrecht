656d04d5d4ea6664c8e3908627cb60f99ff99576
Robbert <mail@robbertbroersma.nl>
2021-07-07 21:58:58 +0000

    Deploying to gh-pages from @ nl-design-system/utrecht@8daad1dea70338cfee0c1f95f026839b4286d2c3 🚀

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 /**
  * Do not edit directly
- * Generated on Wed, 07 Jul 2021 19:51:23 GMT
+ * Generated on Wed, 07 Jul 2021 21:57:55 GMT
  */
 
 export const utrechtBadgeDataLetterSpacing = "0.15ch";
